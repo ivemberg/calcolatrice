3fcec013ad234506ce46280de4b6777bc16dfddc
i.bettini <i.bettini@gmail.com>
2022-06-14 12:29:45 +0200

    update

--- calcolatrice/ViewController.swift
+++ calcolatrice/ViewController.swift
@@ -8,7 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var result: UITextField!
+    @IBOutlet var secondNumber: UITextField!
+    @IBOutlet var Operator: UITextField!
+    @IBOutlet var firstNumber: UITextField!
+    
+    
+    @IBAction func Calcola(_ sender: UIButton) {
+        
+        let a = Int(firstNumber.text!)
+        
+        let b = Int(secondNumber.text!)
+        
+        var c = Int(result.text!)
+        
+        if String(Operator.text!) == "+" {
+            
+            c = a! + b!
+            
+        }
+        
+        if String(Operator.text!) == "-" {
+            
+            c = a! - b!
+            
+        }
+        
+        if String(Operator.text!) == "*" {
+            
+            c = a! * b!
+            
+        }
+        
+        if String(Operator.text!) == "/" {
+            
+            c = a! / b!
+            
+        }
+        
+        result.text = String(c!)
+        print(result)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
